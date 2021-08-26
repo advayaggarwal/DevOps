483b72caeb72932d473088a473fbd4fb9e540bd6
advayaggarwal <advayaggarwal0301@gmail.com>
2021-08-26 16:58:44 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# This repository contains assignments for the course DevOps
+
+
+## Authors
+
+- [@advayaggarwal](https://www.github.com/advayaggarwal)
+- [@tushar](https://www.github.com/tusharboruto)
+
+
+## Feedback
+
+If you have any feedback, please reach out to us at advayaggarwal0301@gmail.com
---
+++ merge_sort.cpp
@@ -0,0 +1,93 @@
+/*
+	AUTHOR:			ADVAY AGGARWAL
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define mod		1000000007 //1e9+7	ans%mod
+#define ll 	long long
+#define test_cases(x)	int x; cin>>x; while(x--)
+
+void starter()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef ONLINE_JUDGE
+	//for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	//for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+void merge(int arr[], int l, int mid, int r)
+{
+	//2 pointer approach
+	int temp[r - l + 1];
+	int i = l;
+	int j = mid + 1;
+	//for temp array
+	int k = 0;
+	while (i <= mid && j <= r)
+	{
+		if (arr[i] <= arr[j])
+		{
+			temp[k] = arr[i];
+			i++, k++;
+		}
+		else
+		{
+			temp[k] = arr[j];
+			j++, k++;
+		}
+	}
+	//Elements left in leftmost array, so simply copying them without checking
+	while (i <= mid)
+	{
+		temp[k] = arr[i];
+		i++, k++;
+	}
+	//Elements left in rightmost array, so simply copying them without checking
+	while (j <= r)
+	{
+		temp[k] = arr[j];
+		j++, k++;
+	}
+	//copying elements back to original array
+	//m should be l not 0 because we want ele to be in correct pos wrt original arr not at the 0th index
+	for (int m = l, p = 0; m <= r; m++, p++)
+	{
+		arr[m] = temp[p];
+	}
+}
+//By master's theorem T(n) = 2T(n/2)   +   O(n)   which comes out to be O(nlogn)
+//For						division      merge
+//Time complexity -> O(nlogn) We are doing logn divisions and for each division we are doing one merge operation which is again O(n), so O(nlogn)
+//Worst Case -> O(nlogn)
+//Best Case -> O(nlogn), we are not checking after division whether it's already sorted or not, so merging it in O(n)
+//Not inplace as space complexity -> O(n), we used temp array with max size equal size of arr
+//Stable because at the end we have all single elements at one place and during merge operation we are checking if(arr[i]<=arr[j]) we'll take arr[i]
+void merge_sort(int arr[], int l, int r)
+{
+	if (r > l)
+	{
+		int mid = l + (r - l) / 2;
+		//Divide process
+		merge_sort(arr, l, mid);
+		merge_sort(arr, mid + 1, r);
+		//Merge process
+		merge(arr, l, mid, r);
+	}
+}
+
+int main()
+{
+	starter();
+	int n, i;
+	cin >> n;
+	int arr[n];
+	for (i = 0; i < n; i++)	cin >> arr[i];
+	merge_sort(arr, 0, n - 1);
+	for (i = 0; i < n; i++)	cout << arr[i] << " ";
+	return 0;
+}
